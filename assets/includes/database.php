86b112e8dba774d3cf9ad67ff2d7f9a92c3bc56c
Alison Alecrim <107521413+alisonalecrim@users.noreply.github.com>
2026-01-06 00:15:11 +0000

    commit

--- assets/includes/database.php
+++ assets/includes/database.php
@@ -1,8 +1,8 @@
 <?php
 class Database {
-    private $host = "";  // Host do banco de dados
-    private $db_name = "";  // Nome do banco de dados
-    private $username = "root";  // Usuário do banco de dados
+    private $host = "sql103.infinityfree.com";  // Host do banco de dados
+    private $db_name = "if0_40835529_zayon_uan";  // Nome do banco de dados
+    private $username = "if0_40835529";  // Usuário do banco de dados
     private $password = "";  // Senha do banco de dados
     public $conn;  // A conexão será armazenada aqui
 
